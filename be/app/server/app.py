c9d73b25e878cc2636c7e594d10db1b8eba5e3ca
Bao Tuong Phu <phutuongbao1999@gmail.com>
2022-07-14 19:24:22 +0700

    Config CORS

--- be/app/server/app.py
+++ be/app/server/app.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.server.routes.brand import router as BrandRouter
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(BrandRouter, tags=["Brand"], prefix="/brand")
 
